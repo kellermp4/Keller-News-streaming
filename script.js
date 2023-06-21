204c7a35e225db1c9703915bb3ffdb6edc607fce
kemerphy <121843701+kemerphy@users.noreply.github.com>
2023-06-21 14:19:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,25 @@
 const videoPlayer = document.getElementById('video');
 const videoList = document.querySelectorAll('#video-list ul li a');
 
-// Function to play selected video
+// Function to play video
 function playVideo(event) {
   event.preventDefault();
   const videoUrl = this.getAttribute('href');
   videoPlayer.src = videoUrl;
+  videoPlayer.style.display = 'block';
   videoPlayer.play();
+}
+
+// Function to hide video player
+function hideVideoPlayer() {
+  videoPlayer.style.display = 'none';
 }
 
 // Attach click event listeners to video links
 videoList.forEach(video => {
   video.addEventListener('click', playVideo);
 });
+
+// Add event listeners for video player events
+videoPlayer.addEventListener('ended', hideVideoPlayer);
+videoPlayer.addEventListener('pause', hideVideoPlayer);
