a2de8347c07af52ec21327bc340451d9a85139da
kemerphy <121843701+kemerphy@users.noreply.github.com>
2023-06-21 18:01:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// JavaScript code for video streaming platform
+/ JavaScript code for video streaming platform
 
 // Get references to HTML elements
-const videoPlayer = document.getElementById('video');
-const videoList = document.querySelectorAll('#video-list ul li a');
+const videoPlayer = document.getElementById("video");
+const videoList = document.querySelectorAll("#video-list ul li a");
 
 // Function to play selected video
 function playVideo(event) {
   event.preventDefault();
-  const videoUrl = this.getAttribute('href');
+  const videoUrl = this.getAttribute("href");
   videoPlayer.src = videoUrl;
   videoPlayer.play();
 }
 
 // Attach click event listeners to video links
-videoList.forEach(video => {
-  video.addEventListener('click', playVideo);
+videoList.forEach((video) => {
+  video.addEventListener("click", playVideo);
 });
